2e6011fc6836d4c4c7f79ab7b2a3d979f2af14b7
Alexander <ajspottswood@gmail.com>
2016-10-27 00:14:37 -0400

    minimum functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,46 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+// (function(){
+  angular
+  .module("wdinstagram", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("WdinstagramIndexController", [
+    WdinstagramIndexControllerFunction
+  ])
+  .controller("WdinstagramShowController", [
+    WdinstagramShowControllerFunction
+  ])
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("gramIndex", {
+      url: "/grams",
+      templateUrl: "js/ng-views/index.html",
+      controller: "WdinstagramIndexController",
+      controllerAS: "vm"
+    })
+    .state("gramShow", {
+      url: "grams/:id",
+      templateUrl: "js/ng-views.show.html",
+      controller: "WdinstagramShowController",
+      controllerAS: "vm"
+    })
+  }
+
+  function WdinstagramIndexControllerFunction(){
+    this.grams = [
+      {photoUrl: "Photo"},
+      {author: "Author"},
+      {body: "Body"}
+    ]
+  }
+  function WdinstagramShowControllerFunction(){
+
+  }
+
+// })();
